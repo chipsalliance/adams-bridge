9dbd39ba90d94ca697271303b36f60e61eea7ba1
Michael Norris <michnorris@microsoft.com>
2025-03-26 09:39:57 -0700

    update reg gen script with --cov switch to generate coverage on demand add new verif KAT test that streams msg

--- src/mldsa_top/uvmf/uvmf_template_output/project_benches/mldsa/tb/tests/mldsa_tests_pkg.sv
+++ src/mldsa_top/uvmf/uvmf_template_output/project_benches/mldsa/tb/tests/mldsa_tests_pkg.sv
@@ -49,6 +49,7 @@ package mldsa_tests_pkg;
    `include "src/ML_DSA_keygen_KATs_test.svh"
    `include "src/ML_DSA_keygen_signing_KATs_test.svh"
    `include "src/ML_DSA_verif_KATs_test.svh"
+   `include "src/ML_DSA_verif_KATs_stream_msg_test.svh"
    `include "src/ML_DSA_randomized_verif_test.svh"
    `include "src/ML_DSA_randomized_verif_fail_test.svh"
    `include "src/ML_DSA_randomized_h_decode_fail_test.svh"
