389ea4a3a26b683fc25877f7f058f853bb9eb599
EMRE KARABULUT <63821295+ekarabu@users.noreply.github.com>
2025-07-08 12:10:46 -0400

    [VAL] ML-KEM randomized test env with the reference model (#160)
    * added ML-KEM randomized test env with ref model
    
    * MICROSOFT AUTOMATED PIPELINE: Stamp 'user/ekarabulut/ml-kem-random-test-env' with updated timestamp and hash after successful run
    
    * enabled rndmness
    
    * MICROSOFT AUTOMATED PIPELINE: Stamp 'user/ekarabulut/ml-kem-random-test-env' with updated timestamp and hash after successful run
    
    * MICROSOFT AUTOMATED PIPELINE: Stamp 'user/ekarabulut/ml-kem-random-test-env' with updated timestamp and hash after successful run

--- src/abr_top/uvmf/uvmf_template_output/project_benches/mldsa/tb/tests/mldsa_tests_pkg.sv
+++ src/abr_top/uvmf/uvmf_template_output/project_benches/mldsa/tb/tests/mldsa_tests_pkg.sv
@@ -61,6 +61,9 @@ package mldsa_tests_pkg;
    `include "src/ML_KEM_encaps_KATs_test.svh"
    `include "src/ML_KEM_decaps_KATs_test.svh"
    `include "src/ML_KEM_keygen_decaps_self_check_test.svh"
+   `include "src/ML_KEM_randomized_keygen_test.svh"
+   `include "src/ML_KEM_randomized_encap_test.svh"
+   `include "src/ML_KEM_randomized_decap_test.svh"
 
   // pragma uvmf custom package_item_additional begin
   // UVMF_CHANGE_ME : When adding new tests to the src directory
