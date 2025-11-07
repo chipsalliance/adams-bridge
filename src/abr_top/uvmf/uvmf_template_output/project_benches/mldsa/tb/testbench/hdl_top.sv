6ec08c6119c25267cbeedf531b67db0d0f4cc231
Mojtaba Bisheh Niasar <mojtabab@microsoft.com>
2025-11-07 09:29:51 -0800

    added new DUT for nomasking pipeline

--- src/abr_top/uvmf/uvmf_template_output/project_benches/mldsa/tb/testbench/hdl_top.sv
+++ src/abr_top/uvmf/uvmf_template_output/project_benches/mldsa/tb/testbench/hdl_top.sv
@@ -103,9 +103,19 @@ import kv_defines_pkg::*;
 
   abr_mem_if abr_memory_export();
 
-  // SRAM Latency
-  localparam SRAM_LATENCY = 2;
+  // MASKING_EN
+`ifdef ABR_MASKING_DISABLED
+  localparam MASKING_EN = 0;
+`else
   localparam MASKING_EN = 1;
+`endif
+
+  // SRAM Latency
+`ifdef ABR_SRAM_2CLK_LATENCY
+  localparam SRAM_LATENCY = 2;
+`else
+  localparam SRAM_LATENCY = 1;
+`endif  
 
   // SRAM module
   abr_mem_top #(
@@ -119,6 +129,7 @@ import kv_defines_pkg::*;
 
   // DUT
   abr_top #(
+    .MASKING_EN(MASKING_EN),
     .SRAM_LATENCY(SRAM_LATENCY),
     .MASKING_EN(MASKING_EN),
     .AHB_ADDR_WIDTH(18),
